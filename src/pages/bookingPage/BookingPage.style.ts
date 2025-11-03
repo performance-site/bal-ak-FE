5afda0c76bc9ad85d9b7dad6c4e3d3590e8dd3de
a-neey <2371429@hansung.ac.kr>
2025-11-03 17:41:44 +0900

    💄 Design: 예매 정보 확인 구현

--- src/pages/bookingPage/BookingPage.style.ts
+++ src/pages/bookingPage/BookingPage.style.ts
@@ -5,7 +5,6 @@ export const BookingContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
   gap: 1.5rem;
 `;
 
@@ -15,4 +14,35 @@ export const Line = styled.hr`
   opacity: 0.4;
   background: ${({ theme }) => theme.colors.gray500};
   margin: 0;
+`;
+
+export const InfoInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 0.75rem;
+`;
+
+export const InfoBox = styled.div`
+  display: flex;
+  width: 12.375rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.5rem;
+`;
+
+export const InfoName = styled.p`
+  color: ${({ theme }) => theme.colors.gray100};
+  font-family: Pretendard;
+  font-size: 0.875rem;
+  font-weight: 600;
+  line-height: 140%; /* 1.225rem */
+  letter-spacing: -0.02188rem;
+`;
+
+export const NumberInput = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.375rem;
 `;
--- src/pages/bookingPage/BookingPage.tsx
+++ src/pages/bookingPage/BookingPage.tsx
@@ -4,6 +4,7 @@ import SectionHeader from './components/SectionHeader';
 
 import CheckBox from './components/CheckBox';
 import ShowInfo from './components/ShowInfo';
+import InputBox from './components/InputBox';
 
 const BookingPage = () => {
   return (
@@ -24,6 +25,33 @@ const BookingPage = () => {
       {/* 공연 기본 정보 확인 */}
       <ShowInfo />
       <CheckBox text="확인했습니다." />
+
+      <S.Line />
+
+      {/* 예매 정보 입력 */}
+      <SectionHeader height="2.125rem" title="예매 정보 입력" />
+      <S.InfoInput>
+        <S.InfoBox>
+          <S.InfoName>입금자 성함</S.InfoName>
+          <InputBox variant="medium" placeholder="홍길동" />
+        </S.InfoBox>
+
+        <S.InfoBox>
+          <S.InfoName>입금자 전화번호</S.InfoName>
+          <S.NumberInput>
+            <InputBox variant="small" width="3rem" placeholder="010" />
+            -
+            <InputBox variant="small" width="3.5rem" placeholder="1234" />
+            -
+            <InputBox variant="small" width="3.56rem" placeholder="5678" />
+          </S.NumberInput>
+        </S.InfoBox>
+
+        <S.InfoBox>
+          <S.InfoName>예매 인원</S.InfoName>
+          <InputBox variant="small" placeholder="ex. 1" />
+        </S.InfoBox>
+      </S.InfoInput>
 
       <S.Line />
     </S.BookingContainer>
