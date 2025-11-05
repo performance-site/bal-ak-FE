6ebb0c45638e9acd146b456ba12184f8d6819faf
a-neey <2371429@hansung.ac.kr>
2025-11-05 17:00:37 +0900

    ✨ Feat: 계좌번호 복사 기능 구현

--- src/pages/bookingPage/BookingPage.tsx
+++ src/pages/bookingPage/BookingPage.tsx
@@ -1,4 +1,5 @@
 import * as S from './styles/BookingPage.style';
+import { useState } from 'react';
 
 import SectionHeader from './components/sectionheader/SectionHeader';
 
@@ -6,13 +7,25 @@ import CheckBox from './components/checkbox/CheckBox';
 import ShowInfo from './components/showinfo/ShowInfo';
 import InputBox from './components/inputbox/InputBox';
 import ClickBox from './components/clickbox/ClickBox';
-import { useState } from 'react';
+
+import COPY from '@/assets/booking/copy.svg';
 
 const BookingPage = () => {
+  const account = 'KB국민은행 93770201535579 이진현';
+
   const [selected, setSelected] = useState<string | null>(null);
 
   const handleSelect = (text: string) => {
     setSelected(text);
+  };
+
+  const onCopyClick = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log('계좌번호 복사 성공');
+    } catch (e) {
+      console.log('계좌번호 복사 실패');
+    }
   };
 
   return (
@@ -78,19 +91,36 @@ const BookingPage = () => {
           </S.PriceText>
         </S.PriceTextContainer>
 
-        {/* 송금 방식 선택 */}
-        <S.ClickRow>
-          <ClickBox
-            text="카카오페이 송금"
-            isSelected={selected === '카카오페이 송금'}
-            onClick={() => handleSelect('카카오페이 송금')}
-          />
-          <ClickBox
-            text="네이버페이 송금"
-            isSelected={selected === '네이버페이 송금'}
-            onClick={() => handleSelect('네이버페이 송금')}
-          />
-        </S.ClickRow>
+        {/* 송금 안내 */}
+        <S.PayContainer>
+          {/* 송금 방식 선택 */}
+          <S.ClickRow>
+            <ClickBox
+              text="카카오페이 송금"
+              isSelected={selected === '카카오페이 송금'}
+              onClick={() => handleSelect('카카오페이 송금')}
+            />
+            <ClickBox
+              text="네이버페이 송금"
+              isSelected={selected === '네이버페이 송금'}
+              onClick={() => handleSelect('네이버페이 송금')}
+            />
+          </S.ClickRow>
+
+          <S.CopyBox>
+            <S.CopyText>
+              <S.CopyTitle>직접 계좌이체</S.CopyTitle>
+              <S.CopySub>ㅣ</S.CopySub>
+            </S.CopyText>
+            <S.CopyImg
+              onClick={() => {
+                onCopyClick(account);
+              }}
+              src={COPY}
+            />
+            <S.AddressText>{account}</S.AddressText>
+          </S.CopyBox>
+        </S.PayContainer>
       </S.PriceContainer>
     </S.BookingContainer>
   );
--- src/pages/bookingPage/styles/BookingPage.style.ts
+++ src/pages/bookingPage/styles/BookingPage.style.ts
@@ -104,7 +104,64 @@ export const UserPrice = styled.p`
   }
 `;
 
+export const PayContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.63rem;
+`;
+
 export const ClickRow = styled.div`
   display: flex;
   gap: 0.62rem;
+`;
+
+export const CopyBox = styled.div`
+  display: flex;
+  width: 21.38rem;
+  padding: 0.63rem 1.84rem;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 0.5rem;
+  border: 1px solid ${({ theme }) => theme.colors.buttonGradient1};
+  background: ${({ theme }) => theme.gradients.checkbox};
+`;
+
+export const CopyText = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0;
+`;
+
+export const CopyTitle = styled.p`
+  color: ${({ theme }) => theme.colors.white2};
+  font-family: Pretendard;
+  font-size: 0.75rem;
+  font-weight: 500;
+  line-height: 140%; /* 1.05rem */
+  letter-spacing: -0.01875rem;
+`;
+
+export const CopySub = styled.p`
+  color: ${({ theme }) => theme.colors.white2};
+  font-family: Pretendard;
+  font-size: 0.875rem;
+  font-weight: 500;
+  line-height: 140%; /* 1.225rem */
+  letter-spacing: -0.02188rem;
+`;
+
+export const CopyImg = styled.img`
+  width: 0.79688rem;
+  height: 0.88544rem;
+  flex-shrink: 0;
+  margin: 0.09rem 0.13rem;
+`;
+
+export const AddressText = styled.p`
+  color: ${({ theme }) => theme.colors.gray400};
+  font-family: Pretendard;
+  font-size: 0.75rem;
+  font-weight: 500;
+  line-height: 140%; /* 1.05rem */
+  letter-spacing: -0.01875rem;
 `;
