de37f7f47eff9991a668a437424da764eade4715
a-neey <2371429@hansung.ac.kr>
2025-11-07 16:57:45 +0900

    ♻️ Refactor: ConfirmSection 컴포넌트로 분리

--- src/pages/bookingPage/BookingPage.tsx
+++ src/pages/bookingPage/BookingPage.tsx
@@ -3,26 +3,15 @@ import * as S from './styles/BookingPage.style';
 import { Element } from 'react-scroll';
 
 import SectionHeader from './components/SectionHeader/SectionHeader';
-import ScrollCheck from './components/ScrollCheck/ScrollCheck';
 import UseBookingForm from '../../hooks/UseBookingForm';
 import InfoSection from './components/InfoSection/InfoSection';
 import InputSection from './components/InputSection/InputSection';
 import PriceSection from './components/PriceSection/PriceSection';
+import ConfirmSection from './components/ConfirmSection/ConfirmSection';
 
 const BookingPage = () => {
   const form = UseBookingForm();
-  const {
-    name,
-    phone,
-    member,
-    checked,
-    isAllValid,
-    errorMsgs,
-    setName,
-    setPhone,
-    setMember,
-    toggleCheck,
-  } = form;
+  const { isAllValid, errorMsgs } = form;
 
   return (
     <S.BookingContainer id="bookingScroll">
@@ -55,32 +44,7 @@ const BookingPage = () => {
 
       <Element name="confirm">
         {/* 최종 확인 및 제출 */}
-        <S.ConfirmContainer>
-          <SectionHeader title="최종 확인 및 제출" />
-          <S.ConfirmContent>
-            <S.ConfirmText>
-              <p>[개인정보 수집·이용 동의]</p>
-              <span>
-                입력하신 개인정보는 들불 밴드부 사전 예매 확인 및 공연 안내를
-                위한 용도로만 사용되며, 해당 목적 외의 용도로 이용되지 않습니다.{' '}
-                <br />
-                <br />
-                - 수집 항목: 이름, 전화번호 <br />
-                - 수집 목적: 예매자 확인, 공연 관련 안내 <br />
-                - 보유 기간: 공연 종료 후 7일 이내 파기 <br />- 수집 주체:
-                한성대학교 밴드부 들불
-              </span>
-            </S.ConfirmText>
-          </S.ConfirmContent>
-          <S.ButtonWrapper>
-            <ScrollCheck
-              to="end"
-              text="개인정보 수집·이용에 동의합니다."
-              checked={checked.confirm}
-              onChange={() => toggleCheck('confirm')}
-            />
-          </S.ButtonWrapper>
-        </S.ConfirmContainer>
+        <ConfirmSection form={form} />
       </Element>
 
       <Element name="end">
