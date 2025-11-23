5cd3b5464b5290dbf1171bc4bda02cc3c20d9738
a-neey <2371429@hansung.ac.kr>
2025-11-24 02:05:46 +0900

    ♻️ Refactor: ConfirmSection 반응형 수정

--- src/pages/booking/components/confirmSection/ConfirmSection.style.ts
+++ src/pages/booking/components/confirmSection/ConfirmSection.style.ts
@@ -3,20 +3,20 @@ import styled from 'styled-components';
 export const ConfirmContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 0.81rem;
+  gap: 1.3rem;
 `;
 
 export const ConfirmText = styled.p`
   color: ${({ theme }) => theme.colors.gray6};
   font-family: Pretendard;
-  font-size: 0.8125rem;
+  font-size: 1.3rem;
   font-style: normal;
   font-weight: 700;
   line-height: 140%; /* 1.1375rem */
-  letter-spacing: -0.02031rem;
+  letter-spacing: -0.03rem;
 
   p {
-    margin-bottom: 0.8125rem;
+    margin-bottom: 1.3rem;
   }
 
   span {
@@ -28,5 +28,5 @@ export const ConfirmText = styled.p`
 export const ButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
-  padding-top: 4rem;
+  padding-top: 7.5rem;
 `;
