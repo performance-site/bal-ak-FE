57955e5750c0d835bf1850454b8cc6cf3b984396
a-neey <2371429@hansung.ac.kr>
2025-11-23 22:59:10 +0900

    ✨ Feat: useGetBookingLink 훅 생성

---
+++ src/pages/bookingPage/hooks/useQuery/useGetBookingLink.ts
@@ -0,0 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+import { BookingLinkResponse } from '../../../../types/booking/booking.type';
+import { getBookingLink } from '../../apis/bookingLink';
+
+export const useGetBookingLink = () => {
+  return useQuery<BookingLinkResponse>({
+    queryKey: ['bookingLink', 1],
+    queryFn: async () => {
+      const res = await getBookingLink(1);
+      return res.data;
+    },
+  });
+};
