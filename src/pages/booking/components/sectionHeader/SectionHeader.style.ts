62625d2a1ad3fa3fcf214b09fd44ec493567f52c
a-neey <2371429@hansung.ac.kr>
2025-11-24 02:37:39 +0900

    ♻️ Refactor: SectionHeader 반응형 수정

--- src/pages/booking/components/sectionHeader/SectionHeader.style.ts
+++ src/pages/booking/components/sectionHeader/SectionHeader.style.ts
@@ -12,28 +12,28 @@ export const HeaderTopText = styled.div`
 export const Title = styled.p`
   color: ${({ theme }) => theme.colors.white};
   font-family: Pretendard;
-  font-size: 1.5rem;
+  font-size: 2.4rem;
   font-weight: 600;
   line-height: 140%; /* 33.6px */
-  letter-spacing: -0.6px;
+  letter-spacing: -0.06rem;
 `;
 
 export const QuestionText = styled.p`
   color: ${({ theme }) => theme.colors.gray8};
   text-align: right;
   font-family: Pretendard;
-  font-size: 0.75rem;
+  font-size: 1rem;
   font-weight: 300;
   line-height: 140%; /* 14px */
-  letter-spacing: -0.25px;
+  letter-spacing: -0.025rem;
   text-decoration: underline;
 `;
 
 export const SubTitle = styled.p`
   color: ${({ theme }) => theme.colors.gray2};
-  font-size: 0.75rem;
+  font-size: 1.2rem;
   font-family: Pretendard;
   line-height: 140%; /* 16.8px */
-  letter-spacing: -0.3px;
+  letter-spacing: -0.03rem;
   font-weight: 500;
 `;
