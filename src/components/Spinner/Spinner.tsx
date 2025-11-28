28d8cce7a8361b749643c71842eb10779d77c9c4
양인서 <113487989+sheepyis@users.noreply.github.com>
2025-11-28 19:09:15 +0900

    🎨 Design: 로딩 스피너 모양 변경

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,11 +1,11 @@
-import { ClipLoader } from 'react-spinners';
+import { BeatLoader } from 'react-spinners';
 import * as S from './styles/Spinner.style';
 import theme from '../../styles/theme';
 
 const Spinner = () => {
   return (
     <S.SpinnerContainer>
-      <ClipLoader size={40} color={`${theme.colors.purple5}`} />
+      <BeatLoader size={10} color={`${theme.colors.purple5}`} />
     </S.SpinnerContainer>
   );
 };
