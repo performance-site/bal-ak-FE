9eb957a4ab8a0c75815b2e8ef38a877b74666060
양인서 <113487989+sheepyis@users.noreply.github.com>
2025-11-29 03:05:50 +0900

    ♻️ Refactor: 공연 아이디 환경변수로 접근하기

--- src/apis/setList/setList.ts
+++ src/apis/setList/setList.ts
@@ -1,5 +1,6 @@
 import apiInstance from '../axios/instance';
+import { PERFORMANCE_ID } from '../../config/performance';
 
-export const getSetList = (performanceId: number = 1) => {
+export const getSetList = (performanceId: number = PERFORMANCE_ID) => {
   return apiInstance.get(`/api/performances/${performanceId}/setlist`);
 };
