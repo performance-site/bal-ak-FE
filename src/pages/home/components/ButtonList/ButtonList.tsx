98ebf077189da187458075d05c043a5e1d4fc105
woojo230 <woojunghyck@naver.com>
2025-11-29 23:46:26 +0900

    ✨Feat: 들불 카카오톡 오픈 채팅방 링크 연결

--- src/pages/home/components/ButtonList/ButtonList.tsx
+++ src/pages/home/components/ButtonList/ButtonList.tsx
@@ -4,19 +4,22 @@ import LocationBtn from '../../../../assets/images/home/LocationImg.png';
 import StarBtn from '../../../../assets/images/home/Star.png';
 import setListBtn from '../../../../assets/images/home/PlayImg.png';
 import ButtonItem from './ButtonItem';
-
-export const IconArray = [
-  { src: StarBtn, info: '사전 예매', link: '/booking' },
-  { src: setListBtn, info: '셋리스트', link: '/setlist' },
-  { src: LocationBtn, info: '공연 장소', link: '' },
-  { src: inquiryBtn, info: '문의 사항', link: '/home/1' },
-];
+import { useHomeStore } from '../../../../store/homeStore/homeStore';
 
 interface ButtonListProps {
   onScrollToKakaoMap?: () => void;
 }
 
 function ButtonList({ onScrollToKakaoMap }: ButtonListProps) {
+  const openchatUrl = useHomeStore((state) => state.homeData?.openchatUrl);
+
+  const IconArray = [
+    { src: StarBtn, info: '사전 예매', link: '/booking' },
+    { src: setListBtn, info: '셋리스트', link: '/setlist' },
+    { src: LocationBtn, info: '공연 장소', link: '' },
+    { src: inquiryBtn, info: '문의 사항', link: openchatUrl || '' },
+  ];
+
   return (
     <ButtonListContainer>
       {IconArray.map((item, index) => (
