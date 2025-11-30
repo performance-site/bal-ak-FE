b20a379792ff24724ec59fdd3425609c54ee7191
a-neey <2371429@hansung.ac.kr>
2025-12-01 02:14:44 +0900

    💄 Design: 토스트 메시지 위치 수정

--- src/pages/booking/components/toast/Toast.style.ts
+++ src/pages/booking/components/toast/Toast.style.ts
@@ -9,7 +9,7 @@ export const ToastContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  bottom: 5rem;
+  bottom: 50%;
   background: ${({ theme }) => theme.gradients.toast};
   border-radius: 0.4rem;
   border: 0.01rem solid ${({ theme }) => theme.colors.gray6};
