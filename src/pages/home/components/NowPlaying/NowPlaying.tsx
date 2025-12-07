a0eca8572f9686c33d3a37349bdd94eeadde8acf
woojo230 <woojunghyck@naver.com>
2025-12-07 13:54:26 +0900

    ✨Feat: 현재 공연 곡 안내 메세지 추가

--- src/pages/home/components/NowPlaying/NowPlaying.tsx
+++ src/pages/home/components/NowPlaying/NowPlaying.tsx
@@ -1,12 +1,14 @@
 import * as S from './styles/NowPlaying.style';
 import NowPlayingInfo from './NowPlayingInfo';
 import Player from './Player';
+import NowPlayingNotice from './NowPlayingNotice';
 
 function NowPlaying() {
   return (
     <S.NowPlayingContainer>
       <NowPlayingInfo />
       <Player />
+      <NowPlayingNotice />
     </S.NowPlayingContainer>
   );
 }
---
+++ src/pages/home/components/NowPlaying/styles/NowPlayingNotice.style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Notice = styled.p`
+  font-size: 1rem;
+  font-weight: 500;
+  color: rgba(85, 85, 85, 1);
+  margin-top: 0.8rem;
+`;
