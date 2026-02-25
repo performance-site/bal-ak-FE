cb9d3d703c3e4f02923d80708c413d0867c9f6c3
a-neey <2371429@hansung.ac.kr>
2026-02-25 20:31:41 +0900

    💄 Design: 팀 정보 한 줄에 4명으로 수정

--- src/pages/setListDetail/components/Team/ListTeam/styles/ListTeam.style.ts
+++ src/pages/setListDetail/components/Team/ListTeam/styles/ListTeam.style.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const ListTeamContainer = styled.div`
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(3, 7rem);
+  justify-content: space-between;
   gap: 1rem;
 `;
